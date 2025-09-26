725d654ea88f9cff7b96f544dcd673c3f9411bb3
olive <olive@example.com>
2025-09-27 00:14:45 +0200

    Fix Next.js config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,9 @@
-export default { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+};
+
+export default nextConfig;
